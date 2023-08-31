17607a4f760b9e53f39c46a734627ca979fe3fe8
Justin Hao <justinhao@live.com>
2023-08-31 16:22:21 -0400

    Add fetchMyIP function

---
+++ index.js
@@ -0,0 +1,10 @@
+const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((error, IP) => {
+//   if (error) {
+//     console.log("There was an error!", error);
+//     return;
+//   } else {
+//     console.log("It worked!", IP);
+//   }
+// });
